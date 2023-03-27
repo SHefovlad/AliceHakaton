0e5cf1400af81a43522b8c261c832830584cae59
SHefovlad <97672088+SHefovlad@users.noreply.github.com>
2023-03-28 01:13:51 +0700

    улучшения

--- A.py
+++ A.py
@@ -39,10 +39,15 @@ aliceMoney = 1500
 reset = 0
 reset2 = 0
 
-place = ["Начало", "город Тюмень", "Шанс", "город Самара", "Подоходный налог", "Рижская железная дорога", "город Калуга", "Шанс", "город Пермь", "город Томск", "Тюрьма(просто посетители)", "город Уфа", "Шанс", "город Казань", "город Краснодар", "Курская железная дорога", "город Архангельск", "Шанс", "город Челябинск", "город Нижний Новгород", "Бесплатная стоянка", "город Омск", "Шанс", "город Волгоград", "город Белгород", "Казанская железная дорога", "город Ставрополь", "город Ростов-на-Дону", "Шанс", "город Хабаровск", "В тюрьму", "город Екатеринбург", "город Владивосток", "Шанс", "город Санкт-Петербург", "Ленинградская железная дорога", "Шанс", "город Москва", "Шанс", "город Новосибирск"]
+place = ["Вперёд", "город Тюмень", "Шанс", "город Самара", "Подоходный налог", "Рижская железная дорога", "город Калуга", "Шанс", "город Пермь", "город Томск", "Тюрьма(просто посетители)",
+         "город Уфа", "Шанс", "город Казань", "город Краснодар", "Курская железная дорога", "город Архангельск", "Шанс", "город Челябинск", "город Нижний Новгород", "Бесплатная стоянка",
+         "город Омск", "Шанс", "город Волгоград", "город Белгород", "Казанская железная дорога", "город Ставрополь", "город Ростов-на-Дону", "Шанс", "город Хабаровск", "В тюрьму",
+         "город Екатеринбург", "город Владивосток", "Шанс", "город Санкт-Петербург", "Ленинградская железная дорога", "Шанс", "город Москва", "Шанс", "город Новосибирск"]
+place_level = [-1, 1, -1, 1, -1, 1, 1, -1, 1, 1, -1, 1, -1, 1, 1, 1, 1, -1, 1, 1, -1, 1, -1, 1, 1, 1, 1, 1, -1, 1, -1, 1, 1, -1, 1, 1, -1, 1, -1, 1]
 price = [0, 60, 0, 60, 200, 200, 100, 0, 100, 120, 0, 140, 0, 140, 160, 200, 180, 0, 180, 200, 0, 220, 0, 220, 240, 200, 260, 260, 0, 280, 0, 300, 300, 0, 320, 200, 0, 350, 0, 400]
 luck = [-200, -50, 10, 50, 100, 250, 500]
 playerCard = []
+playerCardPoint = []
 aliceCard = []
 @app.route('/', methods=['POST'])
 
@@ -84,6 +89,7 @@ def resp():
     global playerMoney
     global aliceMoney
     global playerCard
+    global playerCardPoint
     global aliceCard
     global response_text
     global reset
@@ -133,9 +139,11 @@ def resp():
     elif req["request"]["original_utterance"].lower() in ["да", "давай", "поехали", "так точно", "ок", "окей", "погнали", "вперед", "начинай"] and b5 == True:
         startGame = True
         b5 = False
-    # else:
-    #     response_text = 'Извините, я вас плохо поняла, можете сказать по-другому?'
+    #else:
+    #    response_text = 'Извините, я вас плохо поняла, можете сказать по-другому?'
     while startGame == True:
+        response_text = ''
+
         a1 = False
         a2 = False
         a3 = False
@@ -163,10 +171,110 @@ def resp():
         global g8
         global g9
         global g10
+
+        if playerMoney < 0:
+            response_text += 'О нет! Вы - банкрот, а это значит, что я победила! Повезет в следующий раз!'
+            break
+        elif aliceMoney < 0:
+            response_text += 'Я - банкрот. Я проиграла, очень жаль. Ну что ж, мне повезет в другой раз!'
+            break
+
+
         if g4 == True:
-            response_text = 'Отлично! Начинаем игру, скажите "Брось кубики".'
+            response_text += 'Отлично! Начинаем игру, скажите "Брось кубики".'
             g4 = False
         #бросок кубиков пользователя
+        elif req["request"]["original_utterance"].lower() in ["мои карты"]:
+            for i in playerCard:
+                response_text += str(i)
+                response_text += '\n'
+        elif req["request"]["original_utterance"].lower() in ["улучшения"]:
+            response_text += 'Карты, лоступные для улучшения:\n'
+            for i in playerCard:
+                if 5 > place_level[playerCardPoint[playerCard.index(i)]] != -1:
+                    response_text += str(playerCard.index(i) + 1) + ": " + str(i) +  " на уровне " + str(place_level[playerCardPoint[playerCard.index(i)]])
+                    response_text += '\n'
+            response_text += 'Если хотите улучшить какую-то карту, скажите: "Хочу улучшить карту номер..."'
+        elif req["request"]["original_utterance"].lower() in ["хочу улучшить карту номер 1"]:
+            response_text += 'Для улучшения понадобится заплатить 50\n'
+            if playerMoney >= 50:
+                playerMoney -= 50
+                place_level[playerCardPoint[0]] += 1
+                response_text += 'Карта улучшена!'
+            else:
+                response_text += 'Недостаточно средств!'
+        elif req["request"]["original_utterance"].lower() in ["хочу улучшить карту номер 2"]:
+            response_text += 'Для улучшения понадобится заплатить 50\n'
+            if playerMoney >= 50:
+                playerMoney -= 50
+                place_level[playerCardPoint[1]] += 1
+                response_text += 'Карта улучшена!'
+            else:
+                response_text += 'Недостаточно средств!'
+        elif req["request"]["original_utterance"].lower() in ["хочу улучшить карту номер 3"]:
+            response_text += 'Для улучшения понадобится заплатить 50\n'
+            if playerMoney >= 50:
+                playerMoney -= 50
+                place_level[playerCardPoint[2]] += 1
+                response_text += 'Карта улучшена!'
+            else:
+                response_text += 'Недостаточно средств!'
+        elif req["request"]["original_utterance"].lower() in ["хочу улучшить карту номер 4"]:
+            response_text += 'Для улучшения понадобится заплатить 50\n'
+            if playerMoney >= 50:
+                playerMoney -= 50
+                place_level[playerCardPoint[3]] += 1
+                response_text += 'Карта улучшена!'
+            else:
+                response_text += 'Недостаточно средств!'
+        elif req["request"]["original_utterance"].lower() in ["хочу улучшить карту номер 5"]:
+            response_text += 'Для улучшения понадобится заплатить 50\n'
+            if playerMoney >= 50:
+                playerMoney -= 50
+                place_level[playerCardPoint[4]] += 1
+                response_text += 'Карта улучшена!'
+            else:
+                response_text += 'Недостаточно средств!'
+        elif req["request"]["original_utterance"].lower() in ["хочу улучшить карту номер 6"]:
+            response_text += 'Для улучшения понадобится заплатить 50\n'
+            if playerMoney >= 50:
+                playerMoney -= 50
+                place_level[playerCardPoint[5]] += 1
+                response_text += 'Карта улучшена!'
+            else:
+                response_text += 'Недостаточно средств!'
+        elif req["request"]["original_utterance"].lower() in ["хочу улучшить карту номер 7"]:
+            response_text += 'Для улучшения понадобится заплатить 50\n'
+            if playerMoney >= 50:
+                playerMoney -= 50
+                place_level[playerCardPoint[6]] += 1
+                response_text += 'Карта улучшена!'
+            else:
+                response_text += 'Недостаточно средств!'
+        elif req["request"]["original_utterance"].lower() in ["хочу улучшить карту номер 8"]:
+            response_text += 'Для улучшения понадобится заплатить 50\n'
+            if playerMoney >= 50:
+                playerMoney -= 50
+                place_level[playerCardPoint[7]] += 1
+                response_text += 'Карта улучшена!'
+            else:
+                response_text += 'Недостаточно средств!'
+        elif req["request"]["original_utterance"].lower() in ["хочу улучшить карту номер 9"]:
+            response_text += 'Для улучшения понадобится заплатить 50\n'
+            if playerMoney >= 50:
+                playerMoney -= 50
+                place_level[playerCardPoint[8]] += 1
+                response_text += 'Карта улучшена!'
+            else:
+                response_text += 'Недостаточно средств!'
+        elif req["request"]["original_utterance"].lower() in ["хочу улучшить карту номер 10"]:
+            response_text += 'Для улучшения понадобится заплатить 50\n'
+            if playerMoney >= 50:
+                playerMoney -= 50
+                place_level[playerCardPoint[9]] += 1
+                response_text += 'Карта улучшена!'
+            else:
+                response_text += 'Недостаточно средств!'
         elif req["request"]["original_utterance"].lower() in ["брось кубики", "бросай", "кинь кубики", "кидай", "кинь", "брось"] and g1 == True:
             g2 = False
             cubs = random.randint(2, 12)
@@ -175,38 +283,39 @@ def resp():
             if playerPoints >= 40:
                 playerPoints -= 40
                 playerMoney += 200
+                #response_text += 'Вы прошли поле "Вперед" и получаете 200!'
             #определяем не попал ли пользователь на карточку алисы
             elif price[playerPoints] != 0 and place[playerPoints] != place[4]:
                 if place[playerPoints] in aliceCard:
-                    response_text = 'Отлично! Вы попали на мою карточку, и вынуждены заплатить мне 50. Продолжаем?'
+                    response_text += 'Отлично! Вы попали на мою карточку, и вынуждены заплатить мне 50. Продолжаем?'
                     playerMoney -= 50
                 else:
                     #если не попал, то просто говорим инфу о клетке и предлагаем купить
-                    response_text = 'Вам выпало ' + str(cubs) + ' Вы попали на клетку ' + place[playerPoints] + ' цена этой клетки ' + str(price[playerPoints]) + ' ваш бюджет: ' + str(playerMoney) + '. Хотите купить эту клетку?'
+                    response_text += 'Вам выпало ' + str(cubs) + ' Вы попали на клетку ' + place[playerPoints] + ' цена этой клетки ' + str(price[playerPoints]) + ' ваш бюджет: ' + str(playerMoney) + '. Хотите купить эту клетку?'
                 g3 = True
             #узнаем на какие специальные клетки наступил пользователь(все клетки можно посмотреть в массиве)
             elif place[playerPoints] == place[10]:
                 g3 = False
-                response_text = 'Вы попали на клетку ' + place[10] + ', едем дальше?'
+                response_text += 'Вы попали на клетку ' + place[10] + ', едем дальше?'
                 g2 = True
             elif place[playerPoints] == place[20]:
                 g3 = False
-                response_text = 'вы пришли на бесплатную стоянку, Продолжаем игру?'
+                response_text += 'вы пришли на бесплатную стоянку, Продолжаем игру?'
                 g2 = True
             elif place[playerPoints] == place[30]:
                 g3 = False
-                response_text = 'Вы попали на запретную территорию, отправляйтесь в тюрьму и заплатите штраф, разрешите мне пойти?'
+                response_text += 'Вы попали на запретную территорию, отправляйтесь в тюрьму и заплатите штраф, разрешите мне пойти?'
                 playerMoney -= 100
                 playerPoints = 10
                 g2 = True
             elif place[playerPoints] == place[4]:
                 g3 = False
-                response_text = 'Ох, как же вам не повезло, вы вынуждены заплатить подоходный налог, который составить 200, идем вперёд?'
+                response_text += 'Ох, как же вам не повезло, вы вынуждены заплатить подоходный налог в 200, идем вперёд?'
                 playerMoney -= 200
                 g2 = True
             elif place[playerPoints] == place[0]:
                 g3 = False
-                response_text = 'Вы пришли на начало! Продолжаем игру?'
+                response_text += 'Вы пришли в начало! Продолжаем игру?'
                 g2 = True
             #функционал клетки шанс
             else:
@@ -214,19 +323,20 @@ def resp():
                 whyLukc = luck[random.randint(0, 6)]
                 playerMoney += whyLukc
                 if whyLukc < 0:
-                    response_text = 'Сейчас вам не повезло, вы вынуждены отдать ' + str(whyLukc * -1) + ', Продолжаем?'
+                    response_text += 'Вы попали на клетку "Шанс". Сейчас вам не повезло, вы вынуждены отдать ' + str(whyLukc * -1) + ', Продолжаем?'
                 elif whyLukc > 0 and whyLukc <= 100:
-                    response_text = 'Неплохо, вы получаете ' + str(whyLukc) + ', едем дальше?'
+                    response_text += 'Вы попали на клетку "Шанс". Неплохо, вы получаете ' + str(whyLukc) + ', едем дальше?'
                 else:
-                    response_text = 'Вы сказочный везунчик, получите ' + str(whyLukc) + ', идем дальше?'
+                    response_text += 'Вы попали на клетку "Шанс". Вы сказочный везунчик, получите ' + str(whyLukc) + ', идем дальше?'
                 g2 = True
             g1 = False
         #согласие поьлзователя о покупке клетки, пока не сделан отказ
         elif req["request"]["original_utterance"].lower() in ["да", "давай", "поехали", "так точно", "ок", "окей", "погнали", "вперед", "начинай"] and g3 == True:
-            if playerMoney > price[playerPoints]:
-                response_text = 'Отлично! карта: ' + place[playerPoints] + ' успешно куплена! Продолжаем?'
+            if playerMoney >= price[playerPoints]:
+                response_text += 'Отлично! карта: ' + place[playerPoints] + ' куплена! Продолжаем?'
                 playerMoney -= price[playerPoints]
                 playerCard.append(place[playerPoints])
+                playerCardPoint.append(playerPoints)
                 g2 = True
                 g3 = False
                 response = {
@@ -240,7 +350,7 @@ def resp():
                 return response
             #если у пользователя нету денег
             else:
-                response_text = 'Извините, у вас недостаточно средств, что бы купить эту карточку. Играем дальше?'
+                response_text += 'Извините, у вас недостаточно средств, что бы купить эту карточку. Играем дальше?'
                 g2 = True
                 g3 = False
                 response = {
@@ -252,6 +362,19 @@ def resp():
                     'version': request.json["version"]
                 }
                 return response
+        elif req["request"]["original_utterance"].lower() in ["нет", "неа", "не", "отказываюсь"] and g3 == True:
+            response_text += 'Ну что ж, нет так нет. Играем дальше?'
+            g2 = True
+            g3 = False
+            response = {
+                'response': {
+                    'text': response_text,
+                    'end_session': end
+                },
+                'session': request.json["session"],
+                'version': request.json["version"]
+            }
+            return response
         #алиса ходит
         elif req["request"]["original_utterance"].lower() in ["да", "давай", "поехали", "так точно", "ок", "окей", "погнали", "вперед", "начинай"] and g2 == True:
             if alicePoints >= 40:
@@ -263,47 +386,52 @@ def resp():
                 aliceMoney += 200
             #алиса наступила на нашу клетку
             elif price[alicePoints] != 0 and place[alicePoints] != place[4]:
-                if place[alicePoints] in playerCard:
-                    response_text = 'Как же так! Я попала в ваш город и вынуждена отдать вам ..................'
-                    aliceMoney -= 50
+                for i in playerCard:
+                    if place[alicePoints] == i:
+                        response_text += 'Как же так! Я попала в ваш город и вынуждена отдать вам 50'
+                        aliceMoney -= 50 * place_level[playerCardPoint[playerCard.index(i)]]
+                        playerMoney += 50 * place_level[playerCardPoint[playerCard.index(i)]]
+                        break
                 else:
                     #некое подобие ии, здесь алиса решает, покупать ей клетку или нет
                     if aliceMoney - 100 > price[alicePoints]:
-                        response_text = 'Мне выпало ' + str(cubsA) + ' я попала на клетку ' + place[alicePoints] + ' её цена' + str(price[alicePoints]) + ' я могу позволить себе купить её. \n Ваш ход, скажите "Брось кубики".'
+                        response_text += 'Мне выпало ' + str(cubsA) + ' я попала на клетку ' + place[alicePoints] + ' её цена' + str(price[alicePoints]) + ' я могу позволить себе купить её. \n Ваш ход, скажите "Брось кубики".'
                         aliceMoney -= price[alicePoints]
                         aliceCard.append(place[alicePoints]) 
                     else:
-                        response_text = 'Эх, к сожалению я не могу купить эту карточку. \n Ваша очередь ходить, скажите "Брось кубики".'
+                        response_text += 'Эх, к сожалению я не могу купить эту карточку. \n Ваша очередь ходить, скажите "Брось кубики".'
                 g3 = True
             #определяем на какую клетку наступила алиса(все клетки находятся в массиве)
             elif place[alicePoints] == place[10]:
-                response_text = 'Я попала на клетку ' + place[10] + ' \n Ваш черёд ходить, скажите "Брось кубики".'
+                response_text += 'Я попала на клетку ' + place[10] + ' \n Ваш черёд ходить, скажите "Брось кубики".'
                 g3 = False
             elif place[alicePoints] == place[20]:
-                response_text = 'Я пришла на бесплатную стоянку. \n Ваше время пришло, скажите "Брось кубики".'
+                response_text += 'Я пришла на бесплатную стоянку. \n Ваше время пришло, скажите "Брось кубики".'
                 g3 = False
             elif place[alicePoints] == place[30]:
-                response_text = 'Я попала на запретную территорию и вынуждена заплатить штраф. \n Вы все ближе и ближе к победе, скажите "Брось кубики".'
+                response_text += 'Я попала на запретную территорию и вынуждена заплатить штраф. \n Вы все ближе и ближе к победе, скажите "Брось кубики".'
                 aliceMoney -= 100
                 alicePoints = 10
                 g3 = False
             elif place[alicePoints] == place[4]:
-                response_text = 'Как же так! Мне нужно заплатить налог, а вам нежно бросить кубики, как всегда скажите "Брось кубики".'
+                response_text += 'Как же так! Мне нужно заплатить налог, а вам нежно бросить кубики, как всегда скажите "Брось кубики".'
                 aliceMoney -= 200
             elif place[alicePoints] == place[0]:
-                response_text = 'Вот я и пришла в самое начало'
+                response_text += 'Вот я и пришла в самое начало'
             #клетка шанс
             else:
                 whyLukc2 = luck[random.randint(0, 6)]
                 aliceMoney += whyLukc2
                 if whyLukc2 < 0:
-                    response_text = 'Вот невезуха, мне нужно отдать ' + str(whyLukc2 * -1) + ', ваш черёд, скажите "Брось кубики".'
-                elif whyLukc > 0 and whyLukc <= 100:
-                    response_text = 'Отлично, я получаю ' + str(whyLukc2) + ', ваше время пришло, скажите "Брось кубики".'
+                    response_text += 'Вот невезуха, мне нужно отдать ' + str(whyLukc2 * -1) + ', ваш черёд, скажите "Брось кубики".'
+                elif whyLukc2 > 0 and whyLukc2 <= 100:
+                    response_text += 'Отлично, я получаю ' + str(whyLukc2) + ', ваше время пришло, скажите "Брось кубики".'
                 else:
-                    response_text = 'Я попала на клетку "Шанс"! \n Как же мне везет, мне на баланс поступает ' + str(whyLukc2) + ', ваша очередь ходить, скажите "Брось кубики".'
+                    response_text += 'Я попала на клетку "Шанс"! \n Как же мне везет, мне на баланс поступает ' + str(whyLukc2) + ', ваша очередь ходить, скажите "Брось кубики".'
             g1 = True
             g2 = False
+        #else:
+        #    response_text = 'Извините, я вас плохо поняла, можете сказать по-другому?'
         
         response = {
             'response': {
